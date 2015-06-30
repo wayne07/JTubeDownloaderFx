5ebfae4e31b721999759742b5bfa5ffcefbf3c34
juergen seliger <juergen.seliger@gmail.com>
2015-06-30 20:42:52 +0200

    video kann gespeichert werden

--- src/main/java/de/seliger/jtube/CommandBuilder.java
+++ src/main/java/de/seliger/jtube/CommandBuilder.java
@@ -1,22 +1,23 @@
 package de.seliger.jtube;
 
+import com.google.common.collect.Lists;
+
 import java.io.File;
+import java.util.List;
 
 public class CommandBuilder {
 
     //        youtube-dl -o "%(title)s.%(ext)s" -x -k --audio-format mp3 --audio-quality 0 https://www.youtube.com/watch?v=Fm15TdaI2jY
 
     public String[] createCommandArray(String urlToDownload, String workdir, boolean includeVideo) {
-        String[] cmdarray = new String[9];
-        cmdarray[0] = "youtube-dl";
-        cmdarray[1] = "-o";
-        cmdarray[2] = workdir + File.separator + "%(title)s.%(ext)s";
-        cmdarray[3] = "-x";
-        cmdarray[4] = "--audio-format";
-        cmdarray[5] = "mp3";
-        cmdarray[6] = "--audio-quality";
-        cmdarray[7] = "0";
-        cmdarray[8] = urlToDownload;
-        return cmdarray;
+        List<String> commands = Lists.newArrayList("youtube-dl", "-o", workdir + File.separator + "%(title)s.%(ext)s", "-x", "--audio-format", "mp3", "--audio-quality", "0", urlToDownload);
+        if (includeVideo) {
+            commands.add(indexBeforeUrl(commands), "-k");
+        }
+        return commands.toArray(new String[]{});
+    }
+
+    private int indexBeforeUrl(List<String> commands) {
+        return commands.size()-1;
     }
 }
--- src/test/java/de/seliger/jtube/CommandBuilderTest.java
+++ src/test/java/de/seliger/jtube/CommandBuilderTest.java
@@ -8,17 +8,23 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class CommandBuilderTest extends TestCase {
+public class CommandBuilderTest {
 
     private final CommandBuilder commandBuilder = new CommandBuilder();
+    private final String url = "http://iwas.de/bla";
+    private final String workdir = "/tmp/iwo";
 
     @Test
     public void commandWithoutVideo() throws Exception {
-        String url = "http://iwas.de/bla";
-        String workdir ="/tmp/iwo";
-        String[] expectedCommand = new String[]{"youtube-dl", "-o", "%(title)s.%(ext)s", "-x -k --audio-format mp3 --audio-quality 0 https://www.youtube.com/watch?v=Fm15TdaI2jY"};
+        String[] expectedCommand = new String[]{"youtube-dl", "-o", workdir + "/%(title)s.%(ext)s", "-x", "--audio-format", "mp3", "--audio-quality", "0", url};
 
         assertThat(commandBuilder.createCommandArray(url, workdir, false), is(expectedCommand));
+    }
 
+    @Test
+    public void commandWithVideo() throws Exception {
+        String[] expectedCommand = new String[]{"youtube-dl", "-o", workdir + "/%(title)s.%(ext)s", "-x", "--audio-format", "mp3", "--audio-quality", "0", "-k", url};
+
+        assertThat(commandBuilder.createCommandArray(url, workdir, true), is(expectedCommand));
     }
 }
